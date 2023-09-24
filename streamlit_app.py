289986fb70e66d3d5c540584acf5021a452c00af
Artur Salisz <32738266+kramart@users.noreply.github.com>
2023-09-24 11:50:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,3 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
 
-my_cur.execute(
-
-
